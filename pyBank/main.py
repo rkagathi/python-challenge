ddb5f2fca9e93701f5a9d6a552b563b9ba027830
RK <rkagathi@gmail.com>
2018-05-02 21:48:24 -0500

    HW3 files

--- pyBank/main.py
+++ pyBank/main.py
@@ -1,1 +1,57 @@
 # This is the main of pyBank project
+
+import os
+
+# Module for reading CSV's
+import csv
+
+monthRecords = []
+revenueRecords = []
+changeRevenue = []
+
+csv_path = os.path.join('Resources', 'budget_data_2.csv')
+
+with open(csv_path, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+    another_line_after_header = next(csvreader)
+    for row in csvreader:
+        monthRecords.append(row[0])
+        revenueRecords.append(float(row[1]))
+
+# build a list with revenue difference to prior month
+for i in range(len(revenueRecords)):
+    if i > 0:
+        changeRevenue.append(revenueRecords[i] - revenueRecords[i -1])
+    else:
+        changeRevenue.append(0)
+
+print("Financial Analysis")
+print("--------------------------")
+print(f"Total Months {len(monthRecords)} in the list" )    
+print(f"Total Revenue {sum(revenueRecords)} in the list")
+print(f"Average revenue change {round(sum(changeRevenue)/len(changeRevenue),2)}in the list")
+print(f"Greatest Increase in Revenue: {monthRecords[changeRevenue.index(max(changeRevenue))]} {max(changeRevenue)}") 
+print(f"Greatest Decrease in Revenue: {monthRecords[changeRevenue.index(min(changeRevenue))]} {min(changeRevenue)}")
+
+
+
+# txtfile.write(changeRevenue)
+
+
+
+# Specify the file to write to
+output_path = os.path.join( "output", "Budget_Financial_Anaysis.txt")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as txtfile:
+    txtfile.write("Financial Analysis\n")
+    txtfile.write("--------------------------\n")
+    txtfile.write(f"Total Months: {len(monthRecords)} \n" )    
+    txtfile.write(f"Total Revenue: {sum(revenueRecords)} \n")
+    txtfile.write(f"Average revenue change: {round(sum(changeRevenue)/len(changeRevenue),2)}\n")
+    txtfile.write(f"Greatest Increase in Revenue: {monthRecords[changeRevenue.index(max(changeRevenue))]} ( {max(changeRevenue)} )\n") 
+    txtfile.write(f"Greatest Decrease in Revenue: {monthRecords[changeRevenue.index(min(changeRevenue))]} ( {min(changeRevenue)} )\n")
+    
+ 
---
+++ pyPoll/main.py
@@ -0,0 +1,73 @@
+# This is the main of pyBank project
+
+import os
+
+# Module for reading CSV's
+import csv
+
+votorRecords = []
+countyRecords = []
+candidateRecords = []
+
+csv_path = os.path.join('Resources', 'election_data_2.csv')
+
+with open(csv_path, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+    another_line_after_header = next(csvreader)
+    for row in csvreader:
+        votorRecords.append(row[0])
+        countyRecords.append(row[1])
+        candidateRecords.append(row[2])
+
+candidates = list(set(candidateRecords))
+# print(candidates)   
+votes = []
+for candidate in candidates:
+    votes.append(0)
+# print(votes)
+
+# for vote in candidateRecords
+for vote in candidateRecords:
+    # import pdb; pdb.set_trace()
+    votes[candidates.index(vote)] += 1
+
+# print(votes)
+
+print(f"Election Results")
+print(f"----------------------")
+print(f"Total Votes: {len(candidateRecords)}")
+print(f"----------------------")
+for candidate in candidates:
+    print(f"{candidate}: {round(votes[candidates.index(candidate)] * 100 /len(candidateRecords),2)}% ({votes[candidates.index(candidate)]})")
+
+print(f"----------------------")
+print(f"Winner: {candidates[votes.index(max(votes))]}")
+print(f"----------------------")
+
+results = []
+
+results.append(f"Election Results")
+results.append(f"----------------------")
+results.append(f"Total Votes: {len(candidateRecords)}")
+results.append(f"----------------------")
+for candidate in candidates:
+    results.append(f"{candidate}: {round(votes[candidates.index(candidate)] * 100 /len(candidateRecords),2)}% ({votes[candidates.index(candidate)]})")
+
+results.append(f"----------------------")
+results.append(f"Winner: {candidates[votes.index(max(votes))]}")
+results.append(f"----------------------")
+
+
+
+print(results)
+
+outfile = os.path.join('output', 'PollResults.txt')
+
+with open(outfile,"w") as txtfile:
+    for line in results:
+        txtfile.write("\n" + line)
+    
+    
+ 
